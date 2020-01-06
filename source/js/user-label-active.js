5a544f5fdb4c383066995652b42605ec7c0a64f4
Daelamar <redfess@yandex.ru>
2020-01-06 20:37:29 +0300

    Эффекты наведения: навигация,юзер-блок,блок авторизации

---
+++ source/js/user-label-active.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var userBlock = document.querySelector(".user-block");
+var userBlockTags = userBlock.querySelector(".user-block__tags");
+var userBlockInput = userBlockTags.querySelector(".user-block__textarea");
+
+var focusHandler = function () {
+  userBlockTags.classList.add("user-block__tags--active");
+  userBlockInput.addEventListener("focusout", unFocusHandler);
+};
+
+var unFocusHandler = function () {
+  userBlockTags.classList.remove("user-block__tags--active");
+  userBlockInput.addEventListener("focusin", focusHandler);
+};
+
+userBlockInput.addEventListener("focusin", focusHandler);
+userBlockInput.addEventListener("focusout", unFocusHandler);
