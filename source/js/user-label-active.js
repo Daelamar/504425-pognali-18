99e6fa3fa24c3945a569b4d6e37c3efa57bd336e
Daelamar <redfess@yandex.ru>
2020-01-07 14:52:27 +0300

    Доделываю ховеры и фокусы в форме

--- source/js/user-label-active.js
+++ source/js/user-label-active.js
@@ -1,18 +1,22 @@
 "use strict";
 
-var userBlock = document.querySelector(".user-block");
-var userBlockTags = userBlock.querySelector(".user-block__tags");
-var userBlockInput = userBlockTags.querySelector(".user-block__textarea");
+(function () {
+  var userBlock = document.querySelector(".user-block");
+  var userBlockTags = userBlock.querySelector(".user-block__tags");
+  var userBlockInput = userBlockTags.querySelector(".user-block__textarea");
 
-var focusHandler = function () {
-  userBlockTags.classList.add("user-block__tags--active");
-  userBlockInput.addEventListener("focusout", unFocusHandler);
-};
+  var focusHandler = function () {
+    userBlockTags.classList.add("user-block__tags--active");
+    userBlockInput.addEventListener("focusout", unFocusHandler);
+  };
 
-var unFocusHandler = function () {
-  userBlockTags.classList.remove("user-block__tags--active");
+  var unFocusHandler = function () {
+    userBlockTags.classList.remove("user-block__tags--active");
+    userBlockInput.addEventListener("focusin", focusHandler);
+  };
+
   userBlockInput.addEventListener("focusin", focusHandler);
-};
+  userBlockInput.addEventListener("focusout", unFocusHandler);
+}());
 
-userBlockInput.addEventListener("focusin", focusHandler);
-userBlockInput.addEventListener("focusout", unFocusHandler);
+
