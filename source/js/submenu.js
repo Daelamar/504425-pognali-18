b3f5714e31aa3fb553f0401cc22649e41d7e05aa
Daelamar <redfess@yandex.ru>
2020-01-13 22:53:25 +0300

    Минимизировал JS, начал писать скрипты на откр/закр меню

---
+++ source/js/submenu.js
@@ -0,0 +1,21 @@
+"use strict";
+
+(
+  function () {
+    var header = document.querySelector(".main-header");
+    var openButton = header.querySelector(".main-header__button");
+
+    header.classList.remove("main-header--no-js");
+    header.classList.remove("main-header--open");
+
+    var showMenuHandler = function () {
+      if (!header.classList.contains("main-header--open")) {
+        header.classList.add("main-header--open");
+      } else {
+        header.classList.remove("main-header--open");
+      }
+    };
+
+    openButton.addEventListener("click", showMenuHandler);
+  }()
+);
