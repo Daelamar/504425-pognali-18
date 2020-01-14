3403a5e9a826c72a8101d083a5a446ba52118b9f
Daelamar <redfess@yandex.ru>
2020-01-14 23:12:41 +0300

    Исправил валидность

--- source/js/countries.js
+++ source/js/countries.js
@@ -3,9 +3,14 @@
 (function () {
     var countries = document.querySelector(".countries");
     var countriesButton = document.querySelector(".countries__roll");
+    var countriesClose = document.querySelector(".countries__close");
 
     countriesButton.addEventListener("click", function () {
       countries.classList.toggle("countries--open")
+    });
+
+    countriesClose.addEventListener("click", function () {
+      countries.classList.remove("countries--open")
     })
   }()
 );
--- source/js/scroll.js
+++ source/js/scroll.js
@@ -4,13 +4,14 @@
     var header = document.querySelector(".main-header");
 
     var scrollMenuHandler = function () {
-      if (window.pageYOffset > 710 && window.innerWidth >= 1440 || window.pageYOffset > 0 && window.innerWidth < 1440) {
-        header.classList.add("main-header--scroll");
-      } else {
-        header.classList.remove("main-header--scroll");
+      if (!header.classList.contains("main-header--open")) {
+        if (window.pageYOffset > 710 && window.innerWidth >= 1440 || window.pageYOffset > 0 && window.innerWidth < 1440) {
+          header.classList.add("main-header--scroll");
+        } else {
+          header.classList.remove("main-header--scroll");
+        }
       }
     };
-
     window.addEventListener("scroll", scrollMenuHandler);
   }()
 );
